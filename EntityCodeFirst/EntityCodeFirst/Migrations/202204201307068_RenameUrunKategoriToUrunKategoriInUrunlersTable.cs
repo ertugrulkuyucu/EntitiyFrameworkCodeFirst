b2ca7b78647ed6a364880d82f104bb018924ade6
Ertuğrul Kuyucu <75902272+ertugrulkuyucu@users.noreply.github.com>
2022-04-22 23:15:30 +0300

    1

---
+++ EntityCodeFirst/EntityCodeFirst/Migrations/202204201307068_RenameUrunKategoriToUrunKategoriInUrunlersTable.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameUrunKategoriToUrunKategoriInUrunlersTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
